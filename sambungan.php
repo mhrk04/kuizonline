36b68746a5be3f594ec50289df6daf767136fd21
mhrk04 <haziqrohaizan@gmail.com>
2021-04-24 14:49:42 +0800

    tambah file sambungan

---
+++ sambungan.php
@@ -0,0 +1,7 @@
+<?php
+    $host = 'localhost';
+    $user = 'root';
+    $password = '';
+    $database = 'kuizonline';
+    $sambungan = mysqli_connect($host, $user, $password, $database)
+        or die('sambungan gagal');
